a084ddda971bbf4db1ebc7a0ab2f1db6cfbac31f
Tanya <tanyakemkar@gmail.com>
2023-11-21 00:07:03 +0530

    admin change

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -4,12 +4,17 @@ import axios from 'axios';
 
 const Dashboard = () => {
     const [formData, setFormData] = useState(null)
+    const [charge_customers, SetCharge_customers] = useState(false);
     const [categoryData, setCategoryData] = useState([])
     const userId = sessionStorage.getItem('id')
 
     const handleInputChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
     };
+    
 
     // user data 
     const populateUserData = useCallback(async () => {
@@ -19,7 +24,8 @@ const Dashboard = () => {
         const data = await response.json();
   
         if (data.response === 'Success') {
-          setFormData(data.data);
+          SetCharge_customers(data.data.charge_customers);
+          setFormData(data.data.amount);
         } else {
           console.error(data);
           alert(data);
@@ -36,11 +42,11 @@ const Dashboard = () => {
     useEffect(() => {
       if (formData) {
         setCategoryData([
-          formData.amount.category_6,
-          formData.amount.category_7,
-          formData.amount.category_8,
-          formData.amount.category_9,
-          formData.amount.category_10,
+          formData.category_6,
+          formData.category_7,
+          formData.category_8,
+          formData.category_9,
+          formData.category_10,
         ]);
         console.log(formData);
       }
@@ -75,44 +81,36 @@ const Dashboard = () => {
     // admin data put api
     const handleSave = async (e) => {
       e.preventDefault();
-  
+      
       try {
-        const response = await axios.put(
-          'https://stg.dhunjam.in/account/admin/' + userId,
-          formData 
-        );
-      
-        const data = await response.data
+        const response = await axios.put(`https://stg.dhunjam.in/account/admin/${userId}`, {
+          amount: formData.amount,
+        });
   
-        if (data) {
-          console.log(data);
-          
-          populateUserData();
+        // Handle the response data here
+        console.log(response.data);
+        populateUserData();
 
-        } else {
-          alert('Please check your username and password')
-        }
-      
       } catch (error) {
-        console.error('Login failed', error);
+        console.error('Error saving changes:', error);
       }
     };
 
   return (
     <div align="center">
         <h1 className='dashHead'>Social, Hebbal on Dhun Jam</h1>
-        {formData && formData.charge_customers ? 
+        {formData && charge_customers ? 
          <form>
          <div className='dashContent'>
              <div className='charge'>
                  <div className='question'>Do you want to charge your customers for requesting songs?</div>
                  <div className='yes-no'>
                      <div className='radio-button'>
-                         <input type='radio' id='yes' name='yesno' checked={formData.charge_customers} onChange={handleInputChange} />
+                         <input type='radio' id='yes' name='yesno' checked={!formData.charge_customers} onChange={handleInputChange} />
                          <label htmlFor='yes'>Yes</label>
                      </div>
                      <div className='radio-button'>
-                         <input type='radio' id='no' name='yesno' checked={!formData.charge_customers} onChange={handleInputChange} />
+                         <input type='radio' id='no' name='yesno' checked={formData.charge_customers} onChange={handleInputChange} />
                          <label htmlFor='no'>No</label>
                      </div>
                  </div>
@@ -120,32 +118,38 @@ const Dashboard = () => {
              </div>
              <div className='part'>
                  <div className='question'>Custom song request amount-</div>
-                 <input className='button1' 
-                  placeholder='e.g. $60' 
-                  value={formData.amount.category_6} 
-                  onChange={handleInputChange} />
+                 <input
+                  className='button1'
+                  placeholder='e.g. $60'
+                  value={formData.category_6}
+                  onChange={handleInputChange}
+                  name='category_6' />
              </div>
              <div className='part'>
                  <div className='question'>Regular song request amounts, from high to low-</div>
                  <div>
                      <input className='button2' 
                       placeholder='$33' 
-                      value={formData.amount.category_7} 
+                      value={formData.category_7} 
+                      name='category_7'
                       onChange={handleInputChange} />
                      <input 
                       className='button2' 
                       placeholder='$33' 
-                      value={formData.amount.category_8} 
+                      value={formData.category_8} 
+                      name='category_8'
                       onChange={handleInputChange} />
                      <input 
                       className='button2' 
                       placeholder='$33' 
-                      value={formData.amount.category_9} 
+                      value={formData.category_9} 
+                      name='category_9'
                       onChange={handleInputChange} />
                      <input 
                       className='button2' 
                       placeholder='$33' 
-                      value={formData.amount.category_10} 
+                      value={formData.category_10} 
+                      name='category_10'
                       onChange={handleInputChange} />
                  </div>
              </div>
